60ed9c931d40a0e7616aec871a715d56bdae0012
Hiroxz12 <110282352+Hiroxz12@users.noreply.github.com>
2022-08-19 21:33:14 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,13 +3,13 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404:79609055704@s.whatsapp.net
-item1.X-ABLabel:owner selalu ada 
+FN: ᴹᴿ᭄ HiroOffcོ_1ঔৣ꧂
+item.ORG: ᴹᴿ᭄ HiroOffcོ_1ঔৣ꧂
+item1.TEL;waid=6285732267014:6285732267014@s.whatsapp.net
+item1.X-ABLabel:Chat owner insyaallah fast respon 😄
 item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
 item2.X-ABLabel:YOUTUBE
-item3.ADR:;;🎍AMERIKA SERIKAT;;;;
+item3.ADR:;;🎍INDONESIA;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
 item4.URL:https//github.com/Alpiii22 
@@ -25,7 +25,7 @@ const sentMsg  = await conn.sendMessage(
     }
 )
 let jarot = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg'
-await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', 'Store', '.store', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
+await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jangan di spam ya kak...😄', 'Store', '.store', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
 
